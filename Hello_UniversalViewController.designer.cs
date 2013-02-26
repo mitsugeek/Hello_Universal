3616fa92a7d48c1dcb435b3ed94a39e70ba34299
mitsugeek <mitsu322@gmail.com>
2013-02-27 07:23:34 +0900

    add UI Outlet iPad & iPhone

--- Hello_UniversalViewController.designer.cs
+++ Hello_UniversalViewController.designer.cs
@@ -1,5 +1,6 @@
+// WARNING
 //
-// This file has been generated automatically by MonoDevelop to store outlets and
+// This file has been generated automatically by Xamarin Studio to store outlets and
 // actions made in the Xcode designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
@@ -10,8 +11,23 @@ namespace Hello_Universal
 	[Register ("Hello_UniversalViewController")]
 	partial class Hello_UniversalViewController
 	{
+		[Outlet]
+		MonoTouch.UIKit.UIButton btnClickMe { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel lblOutput { get; set; }
+		
 		void ReleaseDesignerOutlets ()
 		{
+			if (btnClickMe != null) {
+				btnClickMe.Dispose ();
+				btnClickMe = null;
+			}
+
+			if (lblOutput != null) {
+				lblOutput.Dispose ();
+				lblOutput = null;
+			}
 		}
 	}
 }
