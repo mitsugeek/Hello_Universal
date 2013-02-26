1c005061cde8afa680e9c39a989875868735fd49
mitsugeek <mitsu322@gmail.com>
2013-02-27 06:51:02 +0900

    create Univasal Single View Application

---
+++ Hello_UniversalViewController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Drawing;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace Hello_Universal
+{
+	public partial class Hello_UniversalViewController : UIViewController
+	{
+		static bool UserInterfaceIdiomIsPhone {
+			get { return UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone; }
+		}
+
+		public Hello_UniversalViewController ()
+			: base (UserInterfaceIdiomIsPhone ? "Hello_UniversalViewController_iPhone" : "Hello_UniversalViewController_iPad", null)
+		{
+		}
+		
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+		
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+		
+		public override bool ShouldAutorotateToInterfaceOrientation (UIInterfaceOrientation toInterfaceOrientation)
+		{
+			// Return true for supported orientations
+			if (UserInterfaceIdiomIsPhone) {
+				return (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
+			} else {
+				return true;
+			}
+		}
+	}
+}
+
